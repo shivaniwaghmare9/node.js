ac32788573c8d7861916140512c14533cbbee6ce
shivani <shivaniw69@gmail.com>
2025-07-21 01:49:08 +0530

    stu

--- routes/stuRoutes.js
+++ routes/stuRoutes.js
@@ -3,15 +3,15 @@ const express=require("express");
 const route=express.Router();
 
 route.get("/name",(req,res)=>{
-    res.send("<h1>hii this is my home page</h1>")
+    res.send("<h1>hii this is my name page</h1>")
 })
 route.get("/rollno",(req,res)=>{
-    res.send("<h1>hii this is my home page</h1>")
+    res.send("<h1>hii this is my rollno page</h1>")
 })
 route.get("/subject",(req,res)=>{
-    res.send("<h1>hii this is my home page</h1>")
+    res.send("<h1>hii this is my subject page</h1>")
 })
 route.get("/fees",(req,res)=>{
-    res.send("<h1>hii this is my home page</h1>")
+    res.send("<h1>hii this is my fees page</h1>")
 })
 module.exports=route;
