c77e615c606dcb9948fb1664a6c1fc5eca32a940
shivani <shivaniw69@gmail.com>
2025-07-21 01:11:26 +0530

    express

--- cybrom.js
+++ cybrom.js
@@ -12,14 +12,28 @@
 // }
 
 //==========================================================USE HTML TAG(H1)=================================================================
-const myName=()=>{
-    return "<h1>this is our node class</h1>"
-}
-const myClass=()=>{
-    return "<h1> i am mern fs developer</h1>"
-}
+// const myName=()=>{
+//     return "<h1>this is our node class</h1>"
+// }
+// const myClass=()=>{
+//     return "<h1> i am mern fs developer</h1>"
+// }
 
-module.exports={
-    myName,
-    myClass
-}
+// module.exports={
+//     myName,
+//     myClass
+// }
+
+//===============================================EXPRESS STATRT HERE=================================================================
+
+const express=require("express")
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello this is my home page</h1>")
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000")
+})
