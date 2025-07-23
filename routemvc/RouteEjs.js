413eb2ef7f36c095dc9ba1ee64639d2d1f9922ff
shivani <shivaniw69@gmail.com>
2025-07-24 01:49:22 +0530

    ejs

--- routemvc/RouteEjs.js
+++ routemvc/RouteEjs.js
@@ -1,6 +1,7 @@
 
 const express=require("express");
 const route=express.Router();
-
+const controllerEjs=require("../controller/controllerEjs")
+route("/",controllerEjs.homepage)
 
 module.exports=route
