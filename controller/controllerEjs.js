1b870701817b387f98d9b5d440eb9817528edde0
shivani <shivaniw69@gmail.com>
2025-07-31 14:49:50 +0530

    controll

--- controller/controllerEjs.js
+++ controller/controllerEjs.js
@@ -1,12 +1,49 @@
 
+// const homepage=(req,res)=>{
+//     res.render("home")
+// }
+// const aboutpage=(req,res)=>{
+//     res.render("about")
+// }
+// const servicepage=(req,res)=>{
+//     res.render("service")
+// }
+// const gallerypage=(req,res)=>{
+//     res.render("gallery")
+// }
+// const contactpage=(req,res)=>{
+//     res.render("contact")
+// }
+
+// module.exports={
+//     homepage,
+//     aboutpage,
+//     servicepage,
+//     gallerypage,
+//     contactpage
+// }
+
+
 const homepage=(req,res)=>{
     res.render("home")
 }
 const aboutpage=(req,res)=>{
     res.render("about")
 }
+const servicepage=(req,res)=>{
+    res.render("service")
+}
+const gallerypage=(req,res)=>{
+    res.render("gallery")
+}
+const contactpage=(req,res)=>{
+    res.render("contact")
+}
 
 module.exports={
     homepage,
-    aboutpage
+    aboutpage,
+    servicepage,
+    gallerypage,
+    contactpage
 }
