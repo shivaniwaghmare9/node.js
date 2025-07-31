316c1a1991080ab22eb62590f7f9ddd1f536e6a8
shivani <shivaniw69@gmail.com>
2025-07-31 14:51:19 +0530

    controll

--- controller/empDatacontrol.js
+++ controller/empDatacontrol.js
@@ -1,25 +1,4 @@
 
-// const namepage=((req,res)=>{
-//     res.send("<h1>Welcome to our name page!!!")
-// })
-// const citypage=((req,res)=>{
-//     res.send("<h1>Welcome to our name page!!!")
-// })
-// const postpage=((req,res)=>{
-//     res.send("<h1>Welcome to our name page!!!")
-// })
-// const salarypage=((req,res)=>{
-//     res.send("<h1>Welcome to our name page!!!")
-// })
-
-// module.exports={
-//        namepage,
-//        citypage,
-//        postpage,
-//        salarypage
-// }
-
-
 const namepage=((req,res)=>{
     res.send("<h1>Welcome to our name page!!!")
 })
