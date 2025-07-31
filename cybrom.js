06abdd0f63d1c5fa4ba4490b29b49a1c68a6c173
shivani <shivaniw69@gmail.com>
2025-07-31 15:00:45 +0530

    cybrom

--- cybrom.js
+++ cybrom.js
@@ -26,31 +26,3 @@
 
 //===============================================EXPRESS STATRT HERE=================================================================
 
-
-// const myName=()=>{
-//     return "this is our node class"
-// }
-// const myClass=()=>{
-//     return " i am mern fs developer"
-// }
-
-// module.exports={
-//     myName,
-//     myClass
-// }
-
-//==========================================================USE HTML TAG(H1)=================================================================
-// const myName=()=>{
-//     return "<h1>this is our node class</h1>"
-// }
-// const myClass=()=>{
-//     return "<h1> i am mern fs developer</h1>"
-// }
-
-// module.exports={
-//     myName,
-//     myClass
-// }
-
-//===============================================EXPRESS STATRT HERE=================================================================
-
