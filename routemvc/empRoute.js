721248dc6ec5e4a7740e19ebe5f73f5a4855318c
shivani <shivaniw69@gmail.com>
2025-07-31 14:52:18 +0530

    route

--- routemvc/empRoute.js
+++ routemvc/empRoute.js
@@ -1,15 +1,4 @@
 
-// const express=require("express");
-// const route=express.Router();
-// const empController=require("../controller/empDatacontrol");
-// route.get("/name",empController.namepage)
-// route.get("/city",empController.citypage)
-// route.get("/post",empController.postpage)
-// route.get("/salary",empController.salarypage)
-
-// module.exports=route;
-
-
 const express=require("express");
 const route=express.Router();
 const empController=require("../controller/empDatacontrol");
