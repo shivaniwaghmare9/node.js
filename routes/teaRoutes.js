9f0d387ad4943f8933d54127553d46bb33ba0e7c
shivani <shivaniw69@gmail.com>
2025-07-31 14:54:18 +0530

    route

--- routes/teaRoutes.js
+++ routes/teaRoutes.js
@@ -1,4 +1,21 @@
 
+// const express=require("express");
+// const route=express.Router();
+
+// route.get("/name",(req,res)=>{
+//     res.send("<h1>hii this is my name page</h1>")
+// })
+// route.get("/post",(req,res)=>{
+//     res.send("<h1>hii this is my post page</h1>")
+// })
+// route.get("/subject",(req,res)=>{
+//     res.send("<h1>hii this is my subject page</h1>")
+// })
+// route.get("/salary",(req,res)=>{
+//     res.send("<h1>hii this is my salary page</h1>")
+// })
+// module.exports=route;
+
 const express=require("express");
 const route=express.Router();
 
